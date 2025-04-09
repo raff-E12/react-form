53db6da60e9e3b82b3bde37a0010c0b6a97fb98e
Raff_12 <raffelecoppola381@gmail.com>
2025-04-09 17:35:59 +0200

    feat: Modifica e delle card è aggiunta di un nuovo componente.

--- src/assets/Cards.jsx
+++ src/assets/Cards.jsx
@@ -8,6 +8,9 @@ export default function Cards() {
         {IsNews.map(element => {
             return(
               <div className='cards' key={element.id}>
+                <div className='cross-icon d-flex'>
+                <i class="fa-solid fa-xmark"></i>
+                </div>
                 {element.immagine !== "" && element.immagine !== undefined ? <div className='box-img-card d-flex' 
                 style={{backgroundImage:`url(${element.immagine})`}}></div> : <div className='box-img-card wall d-flex'><h3>News</h3></div>}
                 <div className='text-card d-flex'>
